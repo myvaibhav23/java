573257665e5ddb334c376c922e9ecc66c828a6ae
vaibhav <warkevaibhav17@gmail.com>
2022-12-05 15:00:40 +0530

    ready

---
+++ Launch.java
@@ -0,0 +1,11 @@
+package launch;
+
+public class Launch {
+
+	public static void main(String[] args) {
+			System.out.println(args[0]);
+			System.out.println(args[1]);
+			System.out.print("hello world");
+	}
+
+}
